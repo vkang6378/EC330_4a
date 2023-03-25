c32ea6cf5b968a75877de0f579b3b27a52e2602c
vkang6378 <vkang@bu.edu>
2023-03-24 21:53:55 -0400

    sorta.cpp

---
+++ sorta.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include "sorta.h"
+
+using namespace std;
+
+std::string sortByFreq(std::string s) {
+    vector<int> freq(128, 0);
+
+    // Count character frequencies
+    for (char count : s) {
+        freq[count]++;
+    }
+
+    // Group the ones with same frequency 
+    vector<vector<char> > group(s.size() + 1);
+
+    for (int i = 0; i < 128; i++) {
+        if (freq[i] > 0) {
+            group[freq[i]].push_back(static_cast<char>(i));
+        }
+    }
+
+    // Sort characters in each group
+    for (auto &group : group) {
+        sort(group.begin(), group.end());
+    }
+
+    // Rewirte the string
+    string sorted_str;
+    for (size_t i = 0; i < group.size(); i++) {
+        for (char c : group[i]) {
+            sorted_str.append(i, c);
+        }
+    }
+
+    return sorted_str;
+}
